e5524c35c38493234d18b96b6434731482924f66
Larry Vincek <larry@vincek.com>
2026-09-15 16:01:32 -0700

    changed /public to /private

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(log);
 
-app.post('/public', auth, mcp);
+app.post('/private', auth, mcp);    // changed so we can keep the no-auth /public alive
 
 app.listen(7020, function() {
     console.log("\n\n\tport 7020 is open for business!\n\n");
